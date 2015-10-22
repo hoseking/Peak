7ea8ff54c66e9f750f75c31736d2a20d9f01b08f
hoseking <shosking@venturemedia.com>
2015-10-22 16:56:08 -0700

    Fix Microphone issues

---
+++ Source/Buffer.swift
@@ -0,0 +1,122 @@
+// Copyright © 2015 Venture Media Labs. All rights reserved.
+//
+// This file is part of Peak. The full Peak copyright notice, including
+// terms governing use, modification, and redistribution, is contained in the
+// file LICENSE at the root of the source code distribution tree.
+
+import Foundation
+
+/// A `Buffer` is similar to an `Array` but it's a `class` instead of a `struct` and it has a fixed size. 
+/// As opposed to an `Array`, assiging a `Buffer` to a new variable will not create a copy, it only creates a new reference.
+/// If any reference is modified all other references will reflect the change. To copy a `Buffer` you have to explicitly call `copy()`.
+public final class Buffer : MutableCollectionType, ArrayLiteralConvertible {
+    public typealias Element = Float32
+    private var buffer: ManagedBuffer<(Int, Int), Element>
+
+    public var count: Int {
+        get {
+            return buffer.value.0
+        }
+        set {
+            buffer.value.0 = newValue
+        }
+    }
+
+    public var capacity: Int {
+        return buffer.value.1
+    }
+
+    public var startIndex: Int {
+        return 0
+    }
+
+    public var endIndex: Int {
+        return count
+    }
+
+    /// A pointer to the RealArray's memory
+    public var pointer: UnsafeMutablePointer<Element> {
+        return buffer.withUnsafeMutablePointerToElements { $0 }
+    }
+
+    /// Construct an uninitialized RealArray of the given size
+    public init(capacity: Int) {
+        buffer = ManagedBuffer<(Int, Int), Element>.create(capacity, initialValue: { _ in (0, capacity) })
+    }
+
+    /// Construct a RealArray from an array literal
+    public convenience init(arrayLiteral elements: Element...) {
+        self.init(capacity: elements.count)
+        pointer.initializeFrom(elements)
+        count = capacity
+    }
+
+    /// Construct a RealArray from an array of reals
+    public convenience init<C : CollectionType where C.Generator.Element == Element>(_ c: C) {
+        self.init(capacity: Int(c.count.toIntMax()))
+        pointer.initializeFrom(c)
+        count = capacity
+    }
+
+    /// Construct a RealArray of `count` elements, each initialized to `repeatedValue`.
+    public convenience init(count: Int, repeatedValue: Element) {
+        self.init(capacity: count)
+        for i in 0..<count {
+            self[i] = repeatedValue
+        }
+        self.count = count
+    }
+
+    public subscript(index: Int) -> Element {
+        get {
+            precondition(0 <= index && index < count)
+            return pointer[index]
+        }
+        set {
+            precondition(0 <= index && index < capacity)
+            pointer[index] = newValue
+        }
+    }
+
+    public func copy() -> Buffer {
+        let copy = Buffer(capacity: count)
+        copy.pointer.initializeFrom(pointer, count: count)
+        return copy
+    }
+
+    public func append<C : CollectionType where C.Generator.Element == Element>(c: C) {
+        let p = pointer + count
+        p.initializeFrom(c)
+        count += Int(c.count.toIntMax())
+    }
+
+    public func removeRange(range: Range<Int>) {
+        precondition(range.startIndex < range.endIndex)
+        precondition(0 <= range.startIndex && range.endIndex <= count)
+
+        count -= range.count
+        let start = pointer + range.startIndex
+        let end = pointer + range.endIndex - 1
+        start.assignFrom(end, count: count)
+    }
+}
+
+extension Buffer : CustomStringConvertible, CustomDebugStringConvertible {
+    public var description: String {
+        var string = "["
+        for v in self {
+            string += "\(v.description), "
+        }
+        if string.startIndex.distanceTo(string.endIndex) > 1 {
+            let range = string.endIndex.advancedBy(-2)..<string.endIndex
+            string.replaceRange(range, with: "]")
+        } else {
+            string += "]"
+        }
+        return string
+    }
+
+    public var debugDescription: String {
+        return description
+    }
+}
--- Source/Microphone.swift
+++ Source/Microphone.swift
@@ -9,11 +9,11 @@ import Foundation
 
 public class Microphone {
 
-    private let channels = 1
+    private let sampleSize = UInt32(sizeof(Float32))
+    private let channels = UInt32(1)
     private var inputUnit: AudioUnit = nil
-    private var sampleRate = 44100
-    private var buffer: Array<Double>!
-    private var bufferSize = 512
+    private var sampleRate = Float64(44100)
+    private var buffer: Buffer!
     private var context: Microphone!
 
     private var running = false
@@ -24,15 +24,14 @@ public class Microphone {
     private let inputCallback: AURenderCallback = {(inRefCon, ioActionFlags, inTimeStamp, inBusNumber, inNumberFrames, ioData) -> OSStatus in
         let microphone = UnsafePointer<Microphone>(inRefCon).memory
         if !microphone.stopping {
-            microphone.preload(Int(inNumberFrames), timeStamp: inTimeStamp)
+            microphone.preload(ioActionFlags, inTimeStamp, inBusNumber, inNumberFrames)
         }
         return noErr
     }
 
     public init(sampleRate: Int, bufferSize: Int) {
-        self.sampleRate = sampleRate
-        self.bufferSize = bufferSize
-        self.buffer = Array(count: bufferSize, repeatedValue: 0)
+        self.sampleRate = Float64(sampleRate)
+        self.buffer = Buffer(capacity: bufferSize)
         self.context = self
         create()
     }
@@ -59,11 +58,13 @@ public class Microphone {
         return buffer.count
     }
 
-    public func render(inout data: [Double]) -> Int {
-        let size = min(buffer.count, data.count)
-        data.replaceRange(0..<size, with: buffer[0..<size])
-        buffer.removeRange(0..<size)
-        return size
+    public func render(data: UnsafeMutablePointer<Double>, count: Int) {
+        assert(count <= buffer.count)
+
+        for i in 0..<count {
+            data[i] = Double(buffer[i])
+        }
+        buffer.removeRange(0..<count)
     }
 
     private func create() {
@@ -82,10 +83,11 @@ public class Microphone {
         let component = AudioComponentFindNext(nil, &description)
         checkStatus(AudioComponentInstanceNew(component, &inputUnit), "AudioComponentInstanceNew")
 
-        var disableFlag: UInt32 = 0
-        var enableFlag: UInt32 = 1
         let inputBus: AudioUnitScope = 1
         let outputBus: AudioUnitScope = 0
+
+        var enableFlag: UInt32 = 1
+        var disableFlag: UInt32 = 0
         
         // Enable I/O on microphone input unit
         checkStatus(AudioUnitSetProperty(inputUnit, kAudioOutputUnitProperty_EnableIO, kAudioUnitScope_Input, inputBus, &enableFlag, UInt32(sizeof(enableFlag.dynamicType))), "AudioUnitSetProperty io input")
@@ -95,17 +97,20 @@ public class Microphone {
 
         // Set the stream format
         var streamFormat = AudioStreamBasicDescription()
-        streamFormat.mBitsPerChannel   = UInt32(8 * sizeof(Double))
-        streamFormat.mBytesPerFrame    = UInt32(sizeof(Double))
-        streamFormat.mBytesPerPacket   = UInt32(sizeof(Double))
-        streamFormat.mChannelsPerFrame = UInt32(channels)
+        streamFormat.mBitsPerChannel   = 8 * sampleSize
+        streamFormat.mBytesPerFrame    = sampleSize
+        streamFormat.mBytesPerPacket   = sampleSize
+        streamFormat.mChannelsPerFrame = channels
         streamFormat.mFormatFlags      = kAudioFormatFlagIsFloat | kAudioFormatFlagIsNonInterleaved
         streamFormat.mFormatID         = kAudioFormatLinearPCM
         streamFormat.mFramesPerPacket  = 1
-        streamFormat.mSampleRate       = Double(sampleRate)
+        streamFormat.mSampleRate       = sampleRate
 
         // Set the stream format for output on the microphone's input scope
         checkStatus(AudioUnitSetProperty(inputUnit, kAudioUnitProperty_StreamFormat, kAudioUnitScope_Input, outputBus, &streamFormat, UInt32(sizeof(streamFormat.dynamicType))), "AudioUnitSetProperty stream input")
+
+        // Set the stream format for output on the microphone's output scope
+        checkStatus(AudioUnitSetProperty(inputUnit, kAudioUnitProperty_StreamFormat, kAudioUnitScope_Output, inputBus, &streamFormat, UInt32(sizeof(streamFormat.dynamicType))), "AudioUnitSetProperty stream input")
 
         // Setup input callback
         var callbackStruct = AURenderCallbackStruct(inputProc: inputCallback, inputProcRefCon: &context)
@@ -118,27 +123,35 @@ public class Microphone {
         checkStatus(AudioUnitInitialize(inputUnit), "AudioUnitInitialize")
 
         // Set buffer size
-        var maxFrames = UInt32(bufferSize)
+        var maxFrames = UInt32(buffer.capacity)
         checkStatus(AudioUnitSetProperty(inputUnit, kAudioUnitProperty_MaximumFramesPerSlice, kAudioUnitScope_Global, outputBus, &maxFrames, UInt32(sizeof(maxFrames.dynamicType))), "AudioUnitSetProperty buffer size")
     }
 
-    private func preload(frameCount: Int, timeStamp: UnsafePointer<AudioTimeStamp>) {
-        assert(frameCount <= bufferSize)
+    private func preload(
+        ioActionFlags: UnsafeMutablePointer<AudioUnitRenderActionFlags>,
+        _ inTimeStamp: UnsafePointer<AudioTimeStamp>,
+        _ inOutputBusNumber: UInt32,
+        _ inNumberFrames: UInt32) {
+
+        let numSamples = Int(inNumberFrames)
+        assert(numSamples <= buffer.capacity)
 
         // Discard old data if there is no room
-        if (buffer.count + frameCount > bufferSize) {
-            buffer.removeRange(0..<(buffer.count + frameCount - bufferSize))
+        if (buffer.count + numSamples > buffer.capacity) {
+            buffer.removeRange(0..<(buffer.count + numSamples - buffer.capacity))
         }
 
         // Get new data
         var bufferList = AudioBufferList()
         bufferList.mNumberBuffers = 1
         bufferList.mBuffers.mNumberChannels = 1
-        bufferList.mBuffers.mDataByteSize = UInt32(frameCount * sizeof(Double))
-        bufferList.mBuffers.mData = UnsafeMutablePointer<Void>(UnsafeMutablePointer<Double>(buffer) + buffer.count)
+        bufferList.mBuffers.mDataByteSize = inNumberFrames * sampleSize
+        bufferList.mBuffers.mData = UnsafeMutablePointer<Void>(buffer.pointer + buffer.count)
 
-        var actionFlags = AudioUnitRenderActionFlags(rawValue: 0)
-        checkStatus(AudioUnitRender(inputUnit, &actionFlags, timeStamp, 1, UInt32(frameCount), &bufferList), "AudioUnitRender")
+        checkStatus(AudioUnitRender(inputUnit, ioActionFlags, inTimeStamp, inOutputBusNumber, inNumberFrames, &bufferList), "AudioUnitRender")
+
+        let numSamplesRendered = Int(bufferList.mBuffers.mDataByteSize / sampleSize)
+        buffer.count += numSamplesRendered;
 
         // Notify new data
         dataAvailable?(buffer.count)
@@ -146,7 +159,7 @@ public class Microphone {
 
     private func checkStatus(status: OSStatus, _ message: String) {
         if status == noErr { return }
-        fatalError(message)
+        fatalError("Status: \(status), Message: \(message)")
     }
 
 }
